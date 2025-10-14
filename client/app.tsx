bbd9d83d31ef1557c30e2edcba783b4d1b41a400
Juan Jose <jjalvarez814@gmail.com>
2025-10-13 21:44:56 -0500

    Add profile screen and posibility to edit it

--- client/app.tsx
+++ client/app.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./app/(tabs)/LoginScreen";
+import ProfileScreen from "./app/(tabs)/user_profile";
+import EditProfileScreen from "./app/(tabs)/edit_profile";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Pantalla inicial */}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        {/* Otras pantallas */}
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
