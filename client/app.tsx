aceef865c07676fcd0142f98e9e219cfab2262b5
Alex-sklx0 <david.salazarv@upb.edu.co>
2025-11-13 16:32:39 -0500

    Fix app.json for EAS build

--- client/app.tsx
+++ client/app.tsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./app/(tabs)/LoginScreen";
-import ProfileScreen from "./app/(tabs)/user_profile";
-import EditProfileScreen from "./app/(tabs)/edit_profile";
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* Pantalla inicial */}
-        <Stack.Screen name="Login" component={LoginScreen} />
-        {/* Otras pantallas */}
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-=======
 import { Slot } from 'expo-router';
 import { StripeProvider } from '@stripe/stripe-react-native';
 
@@ -31,6 +9,5 @@ export default function App() {
     <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
       <Slot />
     </StripeProvider>
->>>>>>> 88cf0bff64dc45796733e32b7425fd05109820fa
   );
 }
