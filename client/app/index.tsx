29baa832564985d475a1ad062f7513a478203095
Juan Jose <jjalvarez814@gmail.com>
2025-11-13 02:20:39 -0500

    add new index

---
+++ client/app/index.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+
+/**
+ * Este es el archivo de entrada (ruta raíz /).
+ * No muestra nada a propósito.
+ * El NavigationGuard en app/_layout.tsx lo interceptará
+ * y redirigirá al Login o al Home.
+ */
+export default function Index() {
+  // Se deja vacío a propósito.
+  return <View />;
+}
